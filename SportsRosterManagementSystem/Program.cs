c8c72eddfa3c4e36b3597aad75070fd26e806b60
erikmenchaca <em9927@rit.edu>
2022-11-21 19:24:29 -0500

    first commit

---
+++ SportsRosterManagementSystem/Program.cs
@@ -0,0 +1,39 @@
+﻿//Name: Erik Menchaca
+//Date: November 21, 2022
+//Instructor: Michael Berrios
+//Homework 3: -> Sports Roster Management System
+const string TITLE = "Sports Roster Management System";
+Console.Title = TITLE;
+
+string[] menu =
+{
+    "Create a Team",
+    "Remove a Team",
+    "Change a Team Roster",
+    "List a Team Roster",
+    "List All Teams",
+    "Exit"
+};
+Dictionary<string, Team> teams;
+
+while (true)
+{
+    PrintMenu();
+    System.Console.Write("Select Option: ");
+    int option = int.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+            
+        break;
+    }
+}
+
+void PrintMenu()
+{
+    for (int i = 0; i< menu.Length; i++)
+    {
+        System.Console.WriteLine("{0}. {1}", i +1, menu[i]);
+    }
+}
